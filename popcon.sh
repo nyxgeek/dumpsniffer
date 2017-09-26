316eff35b9cea20142f3b03d595efd3cf9c44ac3
nyxgeek <nyxgeek@rslabs.co>
2017-09-26 00:00:21 -0500

    updated to perform check for password file

--- popcon.sh
+++ popcon.sh
@@ -4,6 +4,32 @@
 DUMPFILE=`grep DUMPFILE dumpsniff.conf | cut -d\= -f2-`
 TMPFILE=`grep TMPFILE dumpsniff.conf | cut -d\= -f2-`
 
+if ! [ -e "$DUMPFILE" ]; then
+	echo "Dump file not found, would you like to download and clean the Troy Hunt dump from hashes.org? (y/n)"
+	echo "Warning: this WILL take a while"
+	read TMPLINE
+	TMPLINE=`echo "$TMPLINE" | tr '[:upper:]' '[:lower:]' `
+
+	if [ "$TMPLINE" == "y" ]; then
+		./getcleanwordlist.sh
+		echo "completed downloading and cleaning"
+		echo "updating dumpsniff.conf to point to new file"
+		sed -i -e 's/^DUMPFILE=.\{0,\}/DUMPFILE=TROYDUMP.v1.txt/' dumpsniff.conf
+		echo "Here is the new dumpsniff.conf:"
+		head dumpsniff.conf
+	else
+		echo "-------------------------------------------------------"
+		echo "Dump file not found - exiting. Please update dumpsniff.conf to point to a file to search."
+		echo "The line should look like:"
+		echo "DUMPFILE=/path/to/dumpfile.txt"
+		echo ""
+		exit 1
+	fi
+
+
+fi
+
+
 #cleanup temp file if it exists
 if [ -e "$TMPFILE" ]; then
 	rm "$TMPFILE"
