00cdb743984b97ff8eb157a7c1362030d5f77007
nyxgeek <nyxgeek@rslabs.co>
2017-09-27 21:38:21 -0500

    fixed cleanup script

--- getcleanwordlist.sh
+++ getcleanwordlist.sh
@@ -2,14 +2,15 @@
 echo "WARNING: THIS WILL TAKE APPROX 2-3GB FREE SPACE TEMPORARILY. Just a heads-up"
 
 #download wordlist
+
 echo "Downloading file now"
 curl -k -o /tmp/troydump_v1.7z "https://hashes.org/download.php?type=leak&id=6505&list=found"
 
-echo "Download complete. Extracting ..."
+echo "Download complete. Extracting ... (this WILL take a while) "
 7z x -o/tmp/ /tmp/troydump_v1.7z
 
 
-grep -x '.\{0,24\}' /tmp/6505_found_sorted.txt | perl -nle 'print if m{^[[:ascii:]]+$}' | grep -v -E '^[[:punct:]]{0,}$'  > TROYDUMP.v1.txt
+grep -x '.\{0,24\}' /tmp/6505_found_sorted.txt | tr -dc '[[:alnum:]][[:space:]][[:punct:]]' | perl -nle 'print if m{^[[:ascii:]]+$}' | grep -v -E '^[[:punct:]]{0,}$' |  sed -e 's/^[ \t]*//' | grep -v '^ *$'  > TROYDUMP.v1.txt
 
 echo "Here is a sample of the text file"
 head TROYDUMP.v1.txt
